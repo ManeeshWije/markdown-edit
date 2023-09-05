5afe8bfec76d0bea0d31abfac94d194ef0908fed
ManeeshWije <m.mwije1@gmail.com>
2023-09-04 21:07:01 -0400

    remove js

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
 export default withMT({
     content: [
         "./src/*.tsx",
