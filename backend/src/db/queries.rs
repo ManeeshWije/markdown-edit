391ec9da592682a1017ab2fc727b2945a3feb33d
ManeeshWije <m.mwije1@gmail.com>
2024-02-19 22:11:25 -0500

    this works but prob need to change pool cloning to something better

--- backend/src/db/queries.rs
+++ backend/src/db/queries.rs
@@ -44,3 +44,56 @@ pub async fn create_user(
         updated_at: Some(user.updated_at.unwrap().to_string()),
     })
 }
+
+pub async fn update_user(
+    pool: &PgPool,
+    uuid: Uuid,
+    username: &str,
+    email: &str,
+) -> Result<User, sqlx::Error> {
+    let user = sqlx::query!(
+        "
+        UPDATE users
+        SET username = $2, email = $3, updated_at = DEFAULT
+        WHERE uuid = $1
+        RETURNING *
+        ",
+        uuid,
+        username,
+        email
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(User {
+        uuid: user.uuid,
+        username: user.username,
+        email: user.email,
+        created_at: Some(user.created_at.unwrap().to_string()),
+        updated_at: Some(user.updated_at.unwrap().to_string()),
+    })
+}
+
+pub async fn delete_user(pool: &PgPool, uuid: Uuid) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "
+        DELETE FROM users
+        WHERE uuid = $1
+        ",
+        uuid
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query!(
+        "
+        DELETE FROM documents
+        WHERE user_uuid = $1
+        ",
+        uuid
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
--- backend/src/routes/users.rs
+++ backend/src/routes/users.rs
@@ -1,8 +1,10 @@
+use std::sync::Arc;
+
 use axum::http::{Response, StatusCode};
 use axum::Json;
 use axum::{
     body::Body,
-    routing::{get, post},
+    routing::{delete, get, post, put},
     Router,
 };
 use uuid::Uuid;
@@ -11,20 +13,34 @@ use crate::db::queries;
 use crate::models::user::User;
 
 pub fn users_routes(pool: sqlx::PgPool) -> Router {
-    let pool_clone = pool.clone();
+    let pool_arc = Arc::new(pool);
+
+    let post_pool_arc = pool_arc.clone();
+    let get_pool_arc = pool_arc.clone();
+    let put_pool_arc = pool_arc.clone();
+    let delete_pool_arc = pool_arc.clone();
+
     Router::new()
         .route(
             "/create",
-            post(move |user: Json<User>| create_user(pool.clone(), user)),
+            post(move |user: Json<User>| create_user(post_pool_arc, user)),
         )
         .route(
             "/:uuid",
-            get(|params: axum::extract::Path<String>| get_user_by_uuid(pool_clone, params)),
+            get(|params: axum::extract::Path<String>| get_user_by_uuid(get_pool_arc, params)),
+        )
+        .route(
+            "/update",
+            put(move |user: Json<User>| update_user(put_pool_arc, user)),
+        )
+        .route(
+            "/delete/:uuid",
+            delete(|params: axum::extract::Path<String>| delete_user(delete_pool_arc, params)),
         )
 }
 
 async fn get_user_by_uuid(
-    pool: sqlx::PgPool,
+    pool: Arc<sqlx::PgPool>,
     params: axum::extract::Path<String>,
 ) -> impl axum::response::IntoResponse {
     // parse the uuid from the path and if its invalid return a 400
@@ -56,7 +72,10 @@ async fn get_user_by_uuid(
         .unwrap()
 }
 
-async fn create_user(pool: sqlx::PgPool, request: Json<User>) -> impl axum::response::IntoResponse {
+async fn create_user(
+    pool: Arc<sqlx::PgPool>,
+    request: Json<User>,
+) -> impl axum::response::IntoResponse {
     let request_body = request.0;
     let uuid = request_body.uuid;
     let username = request_body.username;
@@ -85,4 +104,60 @@ async fn create_user(pool: sqlx::PgPool, request: Json<User>) -> impl axum::resp
         .header("Content-Type", "application/json")
         .body(Body::from(serde_json::to_string(&user).unwrap()))
         .unwrap()
+}
+
+async fn update_user(
+    pool: Arc<sqlx::PgPool>,
+    request: Json<User>,
+) -> impl axum::response::IntoResponse {
+    let request_body = request.0;
+    let uuid = request_body.uuid;
+    let username = request_body.username;
+    let email = request_body.email;
+
+    let user = match queries::update_user(&pool, uuid, username.as_str(), email.as_str()).await {
+        Ok(user) => user,
+        Err(err) => {
+            eprintln!("Database error: {}", err);
+            return Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from("Failed to update user"))
+                .unwrap();
+        }
+    };
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(Body::from(serde_json::to_string(&user).unwrap()))
+        .unwrap()
+}
+
+async fn delete_user(
+    pool: Arc<sqlx::PgPool>,
+    params: axum::extract::Path<String>,
+) -> impl axum::response::IntoResponse {
+    let uuid = match Uuid::parse_str(&params) {
+        Ok(uuid) => uuid,
+        Err(_) => {
+            return Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("Invalid UUID"))
+                .unwrap()
+        }
+    };
+
+    match queries::delete_user(&pool, uuid).await {
+        Ok(_) => Response::builder()
+            .status(StatusCode::OK)
+            .body(Body::from("User deleted"))
+            .unwrap(),
+        Err(err) => {
+            eprintln!("Database error: {}", err);
+            Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from("Failed to delete user"))
+                .unwrap()
+        }
+    }
 }
